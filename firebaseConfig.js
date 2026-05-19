492a8308925e7b23c716b8b3e24356a6e49b45f9
PixelKenneth <77219379+PixelKenneth@users.noreply.github.com>
2026-05-19 00:35:37 -0400

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,1 +1,9 @@
-
+export const firebaseConfig = {
+  apiKey: "AIzaSyDJztBJEVcRAhOpIIgNnUFQ_E2PgAzx49Q",
+  authDomain: "unime-763d2.firebaseapp.com",
+  projectId: "unime-763d2",
+  storageBucket: "unime-763d2.firebasestorage.app",
+  messagingSenderId: "978655202942",
+  appId: "1:978655202942:web:285ba83315b67e10ced22f",
+  measurementId: "G-67CQX9L19N"
+};
